4fe3ecdc4ce60d06c17d2df18732115635ed417c
Robert Pullan <33415358+robpullan85@users.noreply.github.com>
2018-02-25 16:41:14 +0000

    Added functions for additional buttons

--- JavaScript/jQuery.js
+++ JavaScript/jQuery.js
@@ -1,9 +1,31 @@
+
+// This function will operate a slide motion for the text explaining the
+// allowed formats for the calculator
 $(document).ready(function()
 {
 
+// focus the cursor on the expression text box on webpage load
+    $('#inputTextBox').focus();
+
+
+// focus the cursor on the expression text box on clicking the
+// 'Clear Expression' button
+    $( "#clearExpression" ).click(function()
+  {
+    $( "#inputTextBox" ).focus()
+  });
+
+  // focus the cursor on the expression text box on clicking the
+  // 'Clear All' button
+  $( "#clearAll" ).click(function()
+{
+  $( "#inputTextBox" ).focus()
+});
+
+
+// opens the quick guide on how to use the calculator
     $( "#buttonReadMe" ).click(function()
     {
       $( ".readMeTextBox" ).slideToggle();
     });
-
 });
