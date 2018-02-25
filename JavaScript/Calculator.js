2adbb860d22f4bd5add34943f6f5c16f081178a8
Robert Pullan <33415358+robpullan85@users.noreply.github.com>
2018-02-25 01:10:10 +0000

    Further Updates

--- JavaScript/Calculator.js
+++ JavaScript/Calculator.js
@@ -1,77 +1,62 @@
-
-function calculator(input)
-{
+function calculator(input) {
     // global array and variable declared
     var expressionArray = [];
     var divisionByZero = 0;
     var incorrectFormat = 0;
+    var answer = 0;
 
     // while loop to remove any non numbers or operators at the beginning of the string
-    while (isNaN(input.charAt(0)) && (isNaN(input.charAt(1)) || (input.charAt(0)) != '-'))
-    {
-        var myString = input.slice(1);
-        input = myString;
+    while (isNaN(input.charAt(0)) && (isNaN(input.charAt(1)) || (input.charAt(0)) != '-')) {
+        var editedInput = input.slice(1);
+        input = editedInput;
     }
 
     // calling functions to perform the calculation
     creatingArray(input);
 
     // ends the function due to invalid format
-    if (incorrectFormat == 1)
-    {
-        var finalCalculation = 'Incorrect format, check your expression entered.';
-        console.log(finalCalculation);
+    if (incorrectFormat == 1) {
+        var finalCalculation = 'Incorrect format, check your expression.';
         return finalCalculation;
     }
-    else
-    {
+    else {
         orderOfProcedence(expressionArray);
     }
 
     // returning the final answer or an error after all sub expressions have been solved
-    if (divisionByZero == 1)
+    if (divisionByZero == 1 || incorrectFormat == 1)
     {
         var finalCalculation = 'Undefined due to division by zero.';
-        console.log(finalCalculation);
         return finalCalculation;
     }
-    else
-    {
+    else {
         var finalCalculation = expressionArray[0];
-        console.log(finalCalculation);
         return finalCalculation;
     }
-
-
 
 
     // Function for splitting the string into array elements
-    function creatingArray(expression)
-    {
+    function creatingArray(expression) {
         // initial variables declared
-        var Operator = '/*+-';
+        var operator = '/*+-';
+        var nonOperator = '/*';
         var term = '';
 
         // iteration to loop through the string
-        for (var i = 0, j; j = expression.charAt(i); i++)
-        {
+        for (var i = 0, j; j = expression.charAt(i); i++) {
             // first if statement to check for a number, also works for decimal places
-            if ((!isNaN(j) || ((j == '.' && (expression.charAt(i + 1) != '.' || Operator.indexOf(expression.charAt(i + 1) == -1))))))
-            {
+            if ((!isNaN(j) || ((j == '.' && (expression.charAt(i + 1) != '.' || operator.indexOf(expression.charAt(i + 1) == -1)))))) {
                 term += j;
 
                 // second if statement to check how big the number is by looking for the next operator inline
-                if (Operator.indexOf(expression.charAt(i + 1)) > -1)
-                {
+                if (operator.indexOf(expression.charAt(i + 1)) > -1) {
                     var k = 0;
                     // checking the term variable for multiple decimal places
                     for (var g = 0, h; h = term.charAt(g); g++) {
-                        if (h == '.')
-                        {
+                        if (h == '.') {
                             k++;
                         }
-                        if (k > 1)
-                        {
+                        if (k > 1) {
                             incorrectFormat = 1;
                             return false;
                         }
@@ -82,78 +67,62 @@ function calculator(input)
                 }
 
                 // continues the loop should the number be larger than a single digit
-                else
-                {
-                   continue;
+                else {
+                    continue;
                 }
             }
 
-            // Checks for two negative numbers which makes a positive number
-            else if ((Operator.indexOf(j) == 3 && expression.charAt(i + 1) == '-') || (Operator.indexOf(j) == 2 && expression.charAt(i + 1) == '+'))
+            // Checks for two negative signs to form a positive number
+            else if ((operator.indexOf(j) == 3 && expression.charAt(i + 1) == '-') || (operator.indexOf(j) == 2 && expression.charAt(i + 1) == '+') || (operator.indexOf(j) == 3 && expression.charAt(i - 1) == '-') || (operator.indexOf(j) == 2 && expression.charAt(i - 1) == '+'))
             {
-                if (expressionArray[expressionArray.length - 1] == Operator.charAt(2) || expressionArray[expressionArray.length - 1] == Operator.charAt(3))
+                if (expressionArray[expressionArray.length - 1] == operator.charAt(2) || expressionArray[expressionArray.length - 1] == operator.charAt(3))
                 {
                     expressionArray.pop();
                 }
 
                 expressionArray.push('+');
                 term = '';
-
                 //expression = expression.slice(i);
                 continue;
             }
 
+
             // Checks for alternate minus and positive numbers
-            else if ((Operator.indexOf(j) == 2 && expression.charAt(i + 1) == '-') || (Operator.indexOf(j) == 3 && expression.charAt(i + 1) == '+'))
+            else if ((operator.indexOf(j) == 2 && expression.charAt(i + 1) == '-') || (operator.indexOf(j) == 3 && expression.charAt(i + 1) == '+') || (operator.indexOf(j) == 2 && expression.charAt(i - 1) == '-') || (operator.indexOf(j) == 3 && expression.charAt(i - 1) == '+'))
             {
-                if (expressionArray[expressionArray.length - 1] == Operator.charAt(3) || expressionArray[expressionArray.length - 1] == Operator.charAt(2))
+                if (expressionArray[expressionArray.length - 1] == operator.charAt(3) || expressionArray[expressionArray.length - 1] == operator.charAt(2))
                 {
                     expressionArray.pop();
                 }
                 expressionArray.push('-');
                 term = '';
-
                 //expression = expression.slice(i);
                 continue;
             }
 
-            // Checks for a valid operator and empties the term variable
-            else if (Operator.indexOf(j) > -1)
+            // Checks for a valid operators and empties the term variable
+            else if (operator.indexOf(j) > -1 && nonOperator.indexOf(expression.charAt(i + 1)) == -1)
             {
 
-                // Final check for two negative numbers to make positive number
-                if ((Operator.indexOf(j) == 3 && expressionArray[expressionArray.length - 1] == '-') || (Operator.indexOf(j) == 2 && expressionArray[expressionArray.length - 1] == '+'))
-                {
-                    expressionArray.pop();
-                    expressionArray.push('+');
-                    term = '';
-                }
+                expressionArray.push(j);
+                term = '';
 
-                // Final check for two alternate minus and positive number
-                else if ((Operator.indexOf(j) == 3 && expressionArray[expressionArray.length - 1] == '+') || (Operator.indexOf(j) == 2 && expressionArray[expressionArray.length - 1] == '-'))
-                {
-                    expressionArray.pop();
-                    expressionArray.push('-');
-                    term = '';
-                }
-                else
-                {
-                    expressionArray.push(j);
-                    term = '';
-                }
-
-                //Checks for any negative integers
-                if (expression.charAt(i + 1) == '-')
-                {
+                //Checks for any negative signs after an operator
+                if (expression.charAt(i + 1) == '-') {
                     term += '-';
+                    i++;
+                    continue;
+                }
+                // Checks for any postive signs after an operator
+                if (expression.charAt(i + 1) == '+') {
+                    term += '+';
                     i++;
                     continue;
                 }
             }
 
             // Invalid format for this calculator
-            else
-            {
+            else {
                 incorrectFormat = 1;
                 return false;
             }
@@ -161,12 +130,10 @@ function calculator(input)
         }
 
         // Checks for a ambiguous minus sign at the beginning of the array
-        if (expressionArray[0] === '-')
-        {
+        if (expressionArray[0] == '-') {
             var firstElement = '-' + expressionArray[1];
             expressionArray.splice(0, 2, parseFloat(firstElement));
         }
-        console.log(expressionArray);
         return expressionArray
     }
 
@@ -185,8 +152,7 @@ function calculator(input)
                 {
                     return false;
                 }
-                else
-                {
+                else {
                     i = 0;    // resets the counter to begin from the start of the expression
                 }
 
@@ -221,19 +187,17 @@ function calculator(input)
         // declaring the variables required
         var a = expressionArray[elementPosition - 1];   // selecting the first term
         var b = expressionArray[elementPosition + 1];   // selecting the second term
-        var elementDelete = elementPosition - 1;        // selecting the start position to delete the elements resolved
-        var answer = 0;
+        var elementDelete = elementPosition - 1;        // selecting the start position to delete the elements resolved        
 
         // this will perform the calculation per term
-        switch (sign) {
+        switch (sign)
+        {
             // division case and checks for division by zero
             case '/':
-                if (b == 0)
-                {
+                if (b == 0) {
                     divisionByZero = 1;
                 }
-                else
-                {
+                else {
                     answer = a / b;
                 }
                 break;
@@ -251,7 +215,13 @@ function calculator(input)
                 break;
         }
 
+        // Checks the answer is not a number
+        if (isNaN(answer))
+        {
+            incorrectFormat = 1;
+        }
         expressionArray.splice(elementDelete, 3, answer); // replaces the current sub expression with an answer
+
     }
 
 }
